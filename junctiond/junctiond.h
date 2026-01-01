dad0d06f96ee13ef2f06a375c81499881de819bf
Daniel Xiong <daniel@Daniels-MacBook-Pro-64.local>
2026-01-01 17:15:01 +0800

    updated junctiond and functions

--- junctiond/junctiond.h
+++ junctiond/junctiond.h
@@ -25,6 +25,23 @@ struct FunctionStatus {
     int fd_write; 
     int fd_read;  
 };
+// Represents a job currently running in the background
+struct Job {
+    std::string name;
+    pid_t pid;
+    int fd_write; // stdin of the child
+    int fd_read;  // stdout of the child
+    std::chrono::steady_clock::time_point startTime;
+    bool startupCaptured = false;
+    double startupTime = 0.0;
+};
+
+struct JobResult {
+    std::string name;
+    std::string output;
+    double startupSeconds; // Time from fork to "READY"
+    double totalSeconds;   // Time from fork to exit
+};
 
 class JunctionD {
 public:
@@ -33,13 +50,16 @@ public:
 
     bool spawn(const FunctionData &func);
     bool remove(const std::string &name);
+    JobResult collect(std::string name);
     std::vector<FunctionStatus> list();
 
 private:
     void monitorInstances();
+    
     bool generateConfig(const FunctionData &func, std::string &cfgPath); // declare here
 
     std::map<std::string, FunctionStatus> statusMap;
+    std::vector<Job> activeJobs;
     std::mutex mtx;
     std::thread monitorThread;
 };
