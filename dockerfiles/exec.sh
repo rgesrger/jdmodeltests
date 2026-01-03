38ca71c8ab4362a0b91e578d207932102c59039a
Daniel Xiong <daniel@Daniels-MacBook-Pro-64.local>
2026-01-03 14:23:23 +0800

    fixed exec.sh

--- dockerfiles/exec.sh
+++ dockerfiles/exec.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
 
-echo "[DEBUG] /action/exec start"
-echo "[DEBUG] CWD: $(pwd)"
-echo "[DEBUG] /app contents:"
+# echo "[DEBUG] /action/exec start"
+# echo "[DEBUG] CWD: $(pwd)"
+# echo "[DEBUG] /app contents:"
 ls -lh /app
 
 # Skip stdin for testing
 # INPUT=$(cat)
 # echo "[DEBUG] Received input JSON: $INPUT"
 
-echo "[DEBUG] Running model..."
+# echo "[DEBUG] Running model..."
 OUTPUT=$(/app/distilgpt2_infer /app/distilgpt2.onnx)
-echo "[DEBUG] Model finished"
+# echo "[DEBUG] Model finished"
 
 # Wrap result in JSON
 echo "{\"result\": \"$OUTPUT\"}"
